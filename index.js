690e1046f4e66ef3bc662b17df926cbbe2cef6a4
KarinaDavtyan <k.davtyan27@gmail.com>
2018-01-25 22:40:41 +0100

    feat: auth jwt middleware

--- index.js
+++ index.js
@@ -2,13 +2,34 @@ require('dotenv').config();
 const express = require('express');
 const app = express();
 const cors = require('cors');
+const jwtExpress = require('express-jwt');
+const jwt = require('jsonwebtoken');
 const bodyParser = require('body-parser');
+
 const router = require('./routes.js');
+const User = require('./models/usersModel');
+
+const auth = async (req, res, next) => {
+  const { authorization } = req.headers;
+  if (authorization) {
+    const [strategy, token] = authorization.split(' ');
+    if (strategy !== 'Bearer') return await next();
+    const tokenData = jwt.verify(token, process.env.SECRET)
+    req.user = await User.findOne({username: tokenData.username})
+    await next();
+  } else {
+    await next();
+  }
+}
 
 require('./db');
 app.use(cors());
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+
+app.use(jwtExpress({ secret: process.env.SECRET}).unless({path: ['/new-user', '/sign-in']}));
+
+app.use(auth);
 app.use(router);
 
 
