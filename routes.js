926dc9c6d30773eff9c83547e1b1e9eef5c65a1d
KarinaDavtyan <k.davtyan27@gmail.com>
2018-01-26 00:24:56 +0100

    feat: selection controller

--- routes.js
+++ routes.js
@@ -3,16 +3,27 @@ const router = express.Router();
 
 const usersController = require('./controllers/usersController');
 const wordsController = require('./controllers/wordsController');
+const selectionController = require('./controllers/selectionController');
 
 
 router.post('/user', usersController.postUser);
 router.get('/sign-in', usersController.signIn);
 router.delete('/user', usersController.deleteUser);
 
-router.post('/pair', wordsController.postWords);
-router.get('/words', wordsController.getThePair);
-router.get('/everything', wordsController.getAllWords);
-router.get('/words-user', wordsController.getWordsByUserBot);
+router.post('/selection', selectionController.postSelection);
+router.get('/get-all-selections', selectionController.getAllSelections);
+router.delete('/selection', selectionController.deleteSelection);
+
+
+
+
+
+// router.post('/words', wordsController.postWords);
+// router.get('/words', wordsController.getWords);
+// router.delete('/words', wordsController.deleteWords);
+// router.get('/all-words', wordsController.getAllWords);
+// router.get('/all-words-bot', wordsController.getAllWordsBot);
+// router.get('/selection/:title', selectionController.getSelection);
 
 
 module.exports = router;
