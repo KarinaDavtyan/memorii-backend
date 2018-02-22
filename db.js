61df397656bd0f3385260b4cf461a80439048897
KarinaDavtyan <k.davtyan27@gmail.com>
2018-02-22 09:22:03 +0100

    added mongolab uri

--- db.js
+++ db.js
@@ -4,7 +4,11 @@ mongoose.Promise = global.Promise;
 
 const db = mongoose.connection;
 
-mongoose.connect('mongodb://localhost/plasticTortillaDB');
+let localDB = 'mongodb://localhost/plasticTortillaDB'
+process.env.MONGOLAB_URI
+  ? mongoose.connect(process.env.MONGOLAB_URI)
+  : mongoose.connect(localDB)
 
+console.log(process.env.MONGOLAB_URI);
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', () => console.log('we are connected to plasticTortilla-db'));
